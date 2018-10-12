9cecadf79b98d820a3af6d1253b54de7bd0bee77
DanielSchuller <daniel.schuller@arcor.de>
2018-10-12 23:21:10 +0200

    AdapterUseCase DesignPattern

---
+++ CSharp/AdapterUseCase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    class AdapterUseCase
+    {
+        public AdapterUseCase()
+        {
+
+            Target target = new Adapter();
+            target.Request();
+
+            Console.ReadKey();
+
+        }
+
+        class Target
+
+        {
+            public virtual void Request()
+            {
+                Console.WriteLine("Called Target Request()");
+            }
+        }
+
+
+
+        class Adapter : Target
+
+        {
+            private Adaptee _adaptee = new Adaptee();
+
+            public override void Request()
+            {
+                _adaptee.SpecificRequest();
+            }
+        }
+        class Adaptee
+
+        {
+            public void SpecificRequest()
+            {
+                Console.WriteLine("Called SpecificRequest()");
+            }
+        }
+     }
+        
+    
+}
+
+    
